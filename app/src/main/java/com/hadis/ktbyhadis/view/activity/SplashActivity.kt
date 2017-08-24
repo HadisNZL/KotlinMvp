774f2909d312a7ac1f6ab2d4cd1ae51ee85bc2ce
HadisNZL <hadis_n@163.com>
2017-08-24 15:05:57 +0800

    动态权限添加

--- app/src/main/java/com/hadis/ktbyhadis/view/activity/SplashActivity.kt
+++ app/src/main/java/com/hadis/ktbyhadis/view/activity/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.hadis.ktbyhadis.view.activity
 
+import android.Manifest
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.WindowManager
@@ -8,6 +9,7 @@ import android.view.animation.Animation
 import android.view.animation.AnimationSet
 import android.view.animation.ScaleAnimation
 import com.hadis.ktbyhadis.R
+import com.tbruyelle.rxpermissions2.RxPermissions
 import com.twobbble.tools.newIntent
 import kotlinx.android.synthetic.main.activity_splash.*
 
@@ -19,13 +21,31 @@ class SplashActivity : BaseActivity() {
         setContentView(R.layout.activity_splash)
         //设置全屏
         initView()
-        setAnimation()
+        //setAnimation()
     }
 
     private fun initView() {
         val fontType: Typeface = Typeface.createFromAsset(this.assets, "fonts/Lobster-1.4.otf")
         tv_name_english.typeface = fontType
         tv_english_intro.typeface = fontType
+        initPermissionSet()
+    }
+
+    //base of rxJava2
+    private fun initPermissionSet() {
+        val rxPermissions = RxPermissions(this)
+        rxPermissions
+                .request(Manifest.permission.INTERNET,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                .subscribe { granted ->
+                    if (granted) {
+                        setAnimation()
+                    } else {
+                        setAnimation()
+                    }
+                }
+
     }
 
     private fun setAnimation() {
